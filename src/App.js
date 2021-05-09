b94ca7bb45439b130a627b7ea2671d6ca56e1bc1
Alfredo Martínez <alfre_canos13@icloud.com>
2021-05-09 13:41:03 +0200

    I have styled the About component and added global styles by using styled components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 //Import pages
+import GlobalStyle from './components/GlobalStyle';
 import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div>
+      <GlobalStyle/>
       <AboutUs/>
     </div>
     
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -3,13 +3,14 @@ import React from 'react';
 // Page Components
 import AboutSection from '../components/AboutSection';
 import ServicesSection from '../components/ServicesSection';
-
+import FaqSection from '../components/FaqSection';
 
 const AboutUs = () => {
     return (
         <>
             <AboutSection/>
             <ServicesSection/>
+            <FaqSection/>
         </>
     )
 }
