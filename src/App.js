a8716e0996625b415ed651a23a8b3655338baeaa
Alfredo Martínez <alfre_canos13@icloud.com>
2021-05-09 19:21:01 +0200

    I've created and styled the nav component and imported to the App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 //Import pages
 import GlobalStyle from './components/GlobalStyle';
 import AboutUs from './pages/AboutUs';
+import Nav from './components/Nav'; 
 
 function App() {
   return (
     <div>
       <GlobalStyle/>
+      <Nav/>
       <AboutUs/>
     </div>
     
--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -35,6 +35,7 @@ const GlobalStyle = createGlobalStyle`
     }
     h4 {
         font-weight: bold;
+        font-size: 2rem;
     }
     a {
         font-size: 1.1rem;
