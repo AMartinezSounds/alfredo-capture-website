c5c921796748aae6f77ac61ae33ae75646bf0cda
Alfredo Martínez <alfre_canos13@icloud.com>
2021-05-09 20:04:12 +0200

    Every link of the nav leads to its section. I'm using React Router, so I had to replace every a by Link and every href by to

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 //Import pages
 import GlobalStyle from './components/GlobalStyle';
 import AboutUs from './pages/AboutUs';
+import OurWork from './pages/OurWork'
+import ContactUs from './pages/ContactUs'
 import Nav from './components/Nav'; 
+
+//Router
+import { Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <GlobalStyle/>
       <Nav/>
-      <AboutUs/>
+      <Switch>
+      <Route path="/" exact>
+         <AboutUs/>
+      </Route>
+      <Route path="/work">
+         <OurWork/>
+      </Route>
+      <Route path="/contact">
+         <ContactUs/>
+      </Route>
+      </Switch>
     </div>
     
   );
