3b4a1e478f38ea52dcbdb76d1d7c4435e212804e
Alfredo Martínez <alfre_canos13@icloud.com>
2021-05-09 10:24:52 +0200

    Services section component created and imported to the About Us page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 // Page Components
-
 import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
+
 
 const AboutUs = () => {
     return (
-        <AboutSection/>
-        
+        <>
+            <AboutSection/>
+            <ServicesSection/>
+        </>
     )
 }
 
