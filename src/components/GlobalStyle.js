7041104643e791b667e53725e9b1c26e373031ea
Alfredo Martínez <alfre_canos13@icloud.com>
2021-05-09 16:47:37 +0200

    I've moved the styled components from the AboutSection.js to a new styles.js because they are gonna be used in other components too. So I export each styled component separately and then I import them everywhere I need them. I also have styled the ServicesSection.js

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -8,7 +8,7 @@ const GlobalStyle = createGlobalStyle`
     }
     body {
         background: #1b1b1b;
-
+        font-family: 'Inter', sans-serif;
     }
     button {
         font-weight: bold;
@@ -19,6 +19,7 @@ const GlobalStyle = createGlobalStyle`
         background: transparent;
         color: white;
         transition: all .5s ease;
+        font-family: 'Inter', sans-serif;
         &:hover {
             background-color: #23d997;
             color: white;
