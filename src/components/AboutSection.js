acebf9f86f1d0a41dd1a1cae3238c6c85f6b5040
Alfredo Martínez <alfre_canos13@icloud.com>
2021-05-10 12:55:53 +0200

    I've added animations to the About Us page

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -7,6 +7,9 @@ import { motion } from 'framer-motion';
 
 // import styled from 'styled-components';
 import { About, Description, Image, Hide } from '../styles';
+import { titleAnim, fade, photoAnim } from '../animation';
+
+import Wave from './Wave';
 
 const AboutSection = () => {
 
@@ -15,21 +18,22 @@ const AboutSection = () => {
         <Description>
             <motion.div>
                 <Hide>
-                    <motion.h2>We work to make</motion.h2>
+                    <motion.h2 variants={titleAnim}>We work to make</motion.h2>
                 </Hide>
                 <Hide>
-                    <motion.h2>your <span>dreams</span> come </motion.h2>
+                    <motion.h2 variants={titleAnim}>your <span>dreams</span> come </motion.h2>
                </Hide>
                <Hide>
-                   <motion.h2>true</motion.h2>
+                   <motion.h2 variants={titleAnim}>true</motion.h2>
                </Hide>
             </motion.div>
-             <p>Contact us for any photography or videography that you have. We have professionals with amazing skills</p>
-             <button>Contact Us</button>
+             <motion.p variants={fade}>Contact us for any photography or videography that you have. We have professionals with amazing skills</motion.p>
+             <motion.button variants={fade}>Contact Us</motion.button>
         </Description>
         <Image>
-            <img src={home1} alt="guy with a camera" />
+            <motion.img variants={photoAnim} src={home1} alt="guy with a camera" />
         </Image>
+        <Wave/>
       </About>
     )
 }
