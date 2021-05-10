db504a35d5f09f8fcf29629b4824fbf4274d20cc
Alfredo Martínez <alfre_canos13@icloud.com>
2021-05-10 13:32:18 +0200

    Our Work section animated

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -7,16 +7,25 @@ import theracer from "../img/theracer-small.png";
 import goodtimes from "../img/goodtimes-small.png";
 
 import { motion } from 'framer-motion';
-import { pageAnimation } from '../animation';
+import {sliderContainer, slider, pageAnimation, fade, photoAnim, lineAnim } from '../animation';
 
 function OurWork() {
     return (
         <Work exit='exit' variants={pageAnimation} initial='hidden' animate='show' style={{ background:'#ffffff' }}>
+            <motion.div variants={sliderContainer}>
+                <Frame1 variants={slider}></Frame1>
+                <Frame2 variants={slider}></Frame2>
+                <Frame3 variants={slider}></Frame3>
+                <Frame4 variants={slider}></Frame4>
+            </motion.div>
+            
             <Movie>
-                <h2>The Athlete</h2>
-                <div className="line"></div>
+                <motion.h2 variants={fade}>The Athlete</motion.h2>
+                <motion.div variants={lineAnim} className="line"></motion.div>
                 <Link to='/work/the-athlete'>
-                    <img src={athlete} alt="an athlete" />
+                    <Hide>
+                        <motion.img variants={photoAnim} src={athlete} alt="an athlete" />
+                    </Hide>
                 </Link>
             </Movie>
             <Movie>
@@ -49,7 +58,7 @@ const Movie = styled.div`
     padding-bottom: 10rem;
     .line {
         height: .5rem;
-        background: #cccccc;
+        background: #23d997;
         margin-bottom: 3rem;
     }
     img {
@@ -59,6 +68,32 @@ const Movie = styled.div`
     }
 `;
 
+const Hide = styled.div`
+    overflow: hidden;
+`
 
+// Frame Animation
+
+const Frame1 = styled(motion.div)`
+    position: fixed;
+    left: 0;
+    top: 10%;
+    width: 100%;
+    height: 100vh;
+    background: #fffebf;
+    z-index: 2;
+`;
+
+const Frame2 = styled(Frame1)`
+    background: #ff8efb;
+`;
+
+const Frame3 = styled(Frame1)`
+    background: #8ed2ff;
+`;
+
+const Frame4 = styled(Frame1)`
+    background: #8effa0;
+`;
 
 export default OurWork;
